29701bfe21e5594b4b1185250355268b59c1ff79
Rati Patel <31673893+Rjp76@users.noreply.github.com>
2018-05-08 23:16:19 -0400

    my edits for index.css and index.js

--- index.js
+++ index.js
@@ -2,89 +2,88 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const degree =1800;
+
 
 class Wheel extends React.Component {
-
-	constructor(props) {
+    constructor(props){
         super(props);
-        this.state = {degree: 0, status: null};
-
+        this.state ={degree:0};
+    }
+    handleClick() {
+        this.setState( {degree:this.state.degree+10, });
     }
 
+    render(){
+        let styles = {transform: 'rotate('+this.state.degree+'deg)'};
 
+        return(
+            <div className="wheel" style={styles} onClick={()=>this.handleClick()}>
 
-	handleClick() {
-        let newDegree = degree;
-        let extraDegree = Math.floor(Math.random() * (360 - 1 + 1)) + 1;
-        let totalDegree = newDegree + extraDegree;
-        this.setState({degree: this.state.degree + totalDegree,
-			status: (this.state.degree/360 - Math.trunc(this.state.degree/360)) >= .50 ? "TAILS":"HEADS"});
+                <div className ="section1" >
+                </div>
+                <div className ="section2" >
+                </div>
+                <div className="head">HEADS</div>
+                <div className="tail">TAILS</div>
+                <div className ="spin">
+                    <div className ="innerspin">
+                    </div>
 
+                </div>
+            </div>
+        );
     }
-
-
-
-	render(){
-		let styles = {transform: 'rotate('+this.state.degree+'deg)'};
-
-	  return(
-		<div className = "game">
-	  		<div className="wheel" style={styles} onClick={()=>this.handleClick()}>
-			  
-	  	 	  <div className ="section1" ></div>
-	  	 	  <div className ="section2" ></div>
-			  <div className="head">HEADS</div>
-			  <div className="tail">TAILS</div>
-			  <div className ="spin">
-				<div className ="innerspin"></div>
-		  	  </div>
-			</div>
-			<div className = "results">
-                <h1>{(this.state.degree/360 - Math.trunc(this.state.degree/360)) >= .50 ? "TAILS":"HEADS"}</h1>
-			</div>
-		</div>
-	  );
-	}
 }
-
-
 class Triangle extends React.Component{
-	constructor(props){
-	  super(props);
-	}
-	render(){
-	  return(<div className="triangle" ></div>);
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(<div className="triangle" ></div>);
 
-	}
-}
-
-class Results extends React.Component{
-	constructor(props){
-		super(props);
-	}
-	render(){
-		return(<div className="results"><h1></h1></div>);
-	}
+    }
 }
 
 class Game extends React.Component {
-	render(){
-	  return(
-		<div className="game" >
-		  <Wheel onClick={(i) => this.handleClick(i)} />
-		  <Triangle/>
-			<Results/>
-		</div>
-	  );
-	} 
+    render(){
+        return(
+            <div className="game" >
+
+                <ShowHide className="showhide" />
+            </div>
+        );
+    }
+}
+class ShowHide extends React.Component {
+    constructor () {
+        super()
+        this.state = {
+            isHidden: true
+        }
+    }
+    toggleHidden () {
+        this.setState({
+            isHidden: !this.state.isHidden
+        })
+    }
+    render () {
+        return (
+            <div>
+                {this.state.isHidden && <Wheel onClick={(i) => this.handleClick(i)} /> }
+                {!this.state.isHidden && <Triangle/>}
+                <button className="heck" onClick={this.toggleHidden.bind(this)} disabled={this.state.isHidden}>
+                   Click for Wheel
+                </button>
+                <button className="heck" onClick={this.toggleHidden.bind(this)} disabled={!this.state.isHidden}>
+                    Click for Triangle
+                </button>
+
+            </div>
+        )
+    }
 }
 
 ReactDOM.render(
-  <Game />,
-  document.getElementById('root')
+    <Game />,
+    document.getElementById('root')
 );
-
-
-
-
